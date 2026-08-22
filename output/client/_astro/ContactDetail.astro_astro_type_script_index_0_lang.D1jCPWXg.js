fd08526f3996433b8f09363444c1485e28ad8a99
juan <jjhuertasbotache@gmail.com>
2026-08-21 22:05:21 -0500

    preparing for dev

---
+++ output/client/_astro/ContactDetail.astro_astro_type_script_index_0_lang.D1jCPWXg.js
@@ -0,0 +1,1 @@
+import{t as e}from"./sectionTitleReveal.CvTjsi9L.js";window.__contactHooks={start:()=>{let t=document.querySelector(`#contactDetailedView`);t&&e(t.querySelectorAll(`h2.tron-font`))}};
---
+++ output/client/_astro/page.BAnzaP56.js
@@ -0,0 +1,1 @@
+import{t as e}from"./prefetch.C2dYXND1.js";e();
